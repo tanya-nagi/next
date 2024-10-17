f41f27831d934422f072145d2cc049b98a04ca1a
harsh.kumar <harsh.kumar>
2024-10-18 02:32:02 +0530

    ui

---
+++ src/app/about-us/02_ourTeam.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, FreeMode } from "swiper/modules";
+import Image from "next/image";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "./style.css";
+import SectionTitles from "@components/molecules/sectionTitles";
+
+export default function OurTeam() {
+  return (
+    <section className="co-development sm:blade-top-padding blade-bottom-padding-lg">
+
+      <SectionTitles
+        title='Bringing <span class="font-playfairMedium">product innovation</span> <br/>as a multi-oil, plant-based ingredient house'
+        subtitle='Harnessing global strengths'
+        boldParagraphs={[
+          "AAK India, South Asia, and Sub-Saharan Africa is constantly striving to set new standards in taste, nutrition, and economic viability of products, that people love to consume.",
+        ]}
+        paragraphs={[
+          `Our strategy is one of relentless adaptation; constantly refining our approach to seize new opportunities in product management, customer co-development, and supply chain optimization.`,
+        ]}
+      />
+    </section>
+  );
+}
+
+export const data = [
+  {
+    image: "/assets/coDevelopment/ourTeam/00(3).jpg",
+    id: "1",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(4).jpg",
+    id: "2",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(5).jpg",
+    id: "3",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(6).jpg",
+    id: "4",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(7).jpg",
+    id: "5",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+
+  {
+    image: "/assets/coDevelopment/ourTeam/00(3).jpg",
+    id: "6",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(4).jpg",
+    id: "7",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(5).jpg",
+    id: "8",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(6).jpg",
+    id: "9",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/assets/coDevelopment/ourTeam/00(7).jpg",
+    id: "10",
+    title: "Handover of Ambulance and Garbage",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
---
+++ src/app/about-us/05_plantToBrand.tsx
@@ -0,0 +1,22 @@
+export default function PlantToBrand() {
+  return (
+    <section className="blade-top-padding-lg blade-bottom-padding">
+      <div className="w-container xlg:max-w-screen-xl max-w-screen-lg ">
+        <h1 className=" text-blue font-playfairSemibold text-center ">
+        Better Behaviors
+        </h1>
+        <h6 className="text-sm md:text-lg text-blue mt-3 text-center">
+        The AAK values in motion
+        </h6>
+
+        <p className=" 2xl:my-10 md:my-8 my-5 font-playfairSemibold text-center">
+        Our values are brought to life through our behaviors, guiding us <br/> to make thoughtful decisions and take meaningful actions.
+        </p>
+      </div>
+
+    
+    </section>
+  );
+}
+
+
